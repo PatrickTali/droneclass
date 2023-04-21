bc8cfe752f4033ce8b20faf13562e000f74bd4db
PatrickTali <ptaliaferro218@gmail.com>
2023-04-21 15:19:07 -0400

    dr

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+
+
+
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+
+
+
+class Config():
+    """"""
+
+    FLASK_APP = os.environ.get('Flask_app')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = os.environ.get("Secret_KEY") or "nana nana"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    
+
+
+   
